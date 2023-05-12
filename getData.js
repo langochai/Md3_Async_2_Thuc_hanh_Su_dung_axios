99af6c4874c3f725939633afef50faf139e02586
langochai <langochai11121998@gmail.com>
2023-05-12 14:09:21 +0700

    created

---
+++ getData.js
@@ -0,0 +1,9 @@
+let axios = require('axios')
+function getJSONAPI() {
+    return new Promise(function (resolve) {
+        axios.get('http://jsonplaceholder.typicode.com/posts/1')
+            .then(function (json) {
+                resolve(json.data);
+            });
+    });
+}
